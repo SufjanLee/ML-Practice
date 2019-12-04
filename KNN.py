c66e5829f0d120c5f6efc16c89c778e4e6167159
sufjanlee <814932470@qq.com>
2019-12-04 10:12:17 +0800

    add gridsearchcv for Knn

--- KNN.py
+++ KNN.py
@@ -1,8 +1,8 @@
-import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 def iris_data():
@@ -50,9 +50,34 @@ def knn(x_train, y_train, x_test, y_test):
 	return None
 
 
+def grid(x_train, y_train, x_test, y_test, param_dict):
+	estimator = KNeighborsClassifier()
+	# 网格搜索最优参数
+	estimator = GridSearchCV(estimator, param_grid=param_dict, cv=3)
+	estimator.fit(x_train, y_train)
+
+	# 准确率
+	score = estimator.score(x_test, y_test)
+	prediction = estimator.predict(x_test)
+	print("准确率：\n", score)
+	print("最佳结果：\n", estimator.best_score_)
+	print("最佳估计器: \n", estimator.best_estimator_)
+	print("最佳参数：\n", estimator.best_params_)
+	print("预测值：\n", prediction)
+	print("真实值与预测值比较：\n", prediction == y_test)
+
+	return None
+
+
 if __name__ == '__main__':
 	x_tr, x_te, y_tr, y_te = iris_data()
 	x_tr, x_te = stand(x_tr, x_te)
+	# 未使用网格搜索
 	knn(x_tr, y_tr, x_te, y_te)
-
 	# 测试结果：97.7%
+
+	# 使用网格搜索
+	param_dict = {"n_neighbors": [3, 5, 7, 9, 11, 13, 15]}
+	grid(x_tr, y_tr, x_te, y_te, param_dict)
+
+
