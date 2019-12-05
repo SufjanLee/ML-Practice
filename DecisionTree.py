2ac8dd488b25964bb3d73974a663e5943caf218a
sufjanlee <814932470@qq.com>
2019-12-05 14:40:03 +0800

    decision tree without prunging

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Normalizer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.model_selection import GridSearchCV
 
 
@@ -19,7 +19,7 @@ def iris_data():
 	x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=22)
 	print("训练集数目：\n", x_train.shape)
 	print("测试集数目：\n", x_test.shape)
-	return x_train, x_test, y_train, y_test
+	return x_train, x_test, y_train, y_test, iris
 
 
 def normal(x_train, x_test):
@@ -37,25 +37,26 @@ def normal(x_train, x_test):
 	return x_train, x_test
 
 
-def decision(x_train, x_test, y_train, y_test, param_dict):
-	estimator = DecisionTreeClassifier()
-	estimator = GridSearchCV(estimator, param_grid=param_dict, cv=5)
+def decision(x_train, x_test, y_train, y_test, iris):
+	estimator = DecisionTreeClassifier(criterion="entropy")
 	estimator.fit(x_train, y_train)
 	# 准确率
 	score = estimator.score(x_test, y_test)
 	prediction = estimator.predict(x_test)
 	print("准确率：\n", score)
-	print("最佳结果：\n", estimator.best_score_)
-	print("最佳估计器: \n", estimator.best_estimator_)
-	print("最佳参数：\n", estimator.best_params_)
 	print("预测值：\n", prediction)
 	print("真实值与预测值比较：\n", prediction == y_test)
+	# 对决策树进行可视化，但未解决潜在的过拟合问题（TO-DO）
+	export_graphviz(estimator, out_file="E:\python project\Machine Learning\iris_tree.dot", feature_names=iris.feature_names)
 	return None
 
 
 if __name__ == '__main__':
-	x_tr, x_te, y_tr, y_te = iris_data()
+	x_tr, x_te, y_tr, y_te, ir = iris_data()
 	x_tr, x_te = normal(x_tr, x_te)
+
 	# Supported criteria are “gini” for the Gini impurity and “entropy” for the information gain.
-	param_dict = {"criterion": ["gini", "entropy"]}
-	decision(x_tr, x_te, y_tr, y_te, param_dict)
+	# 未解决过拟合问题，绘制出决策树的构造图
+	decision(x_tr, x_te, y_tr, y_te, ir)
+
+
